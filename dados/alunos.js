181c150ad4150eaa84e1b84e23c4ec09d996eecc
IMenezes <107199192+IMenezes-GH@users.noreply.github.com>
2023-08-08 11:31:40 -0300

    adiciona arquivo alunos.js em dados

---
+++ dados/alunos.js
@@ -0,0 +1,47 @@
+const alunosDB = [
+    {
+    nome : 'Ana',
+    sobrenome: 'Clara',
+    email : 'anaclara1999@email.com',
+    turma : 1,
+    nascimento : new Date(1999, 11, 3),
+    notas: [5, 2, 7, 8],
+    ativo: true,
+    },
+    {
+    nome : 'Beatriz',
+    sobrenome: 'da Silva',
+    email : 'biabiatriz@email.com',
+    turma : 1,
+    nascimento : new Date(1999, 8, 20),
+    notas: [9, 8, 6, 6, 7],
+    ativo: true,
+    },
+    {
+    nome : 'Cláudio',
+    sobrenome: 'Oliveira de Souza',
+    email : 'ccsoujza.12@email.com',
+    turma : 2,
+    nascimento : '2000-01-9',
+    notas: [6, 6, 7, 8, 9],
+    ativo: true,
+    },
+    {
+    nome: 'Maria Clara',
+    sobrenome: 'Franco Barrichelli',
+    email: 'maria.clara.em@email.com',
+    turma: 3,
+    nascimento: new Date(2000, 3, 19),
+    notas: [7, 6, 3, 4],
+    ativo: true
+    },
+    {
+    nome: 'Felipe',
+    sobrenome: 'Alvez de Oliveira',
+    email: 'darkfelipe2000@email.com',
+    turma: 3,
+    nascimento: new Date(2000, 9, 30),
+    notas: [3, 4, 8],
+    ativo: false
+    }
+]
