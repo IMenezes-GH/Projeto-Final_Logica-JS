df059b58e7ba57b59a40307287f7733477ceb748
IMenezes <107199192+IMenezes-GH@users.noreply.github.com>
2023-08-08 15:27:36 -0300

    transfere scripts relacionados aos alunos para seu próprio arquivo e implementa função que valida emails

--- dados/alunos.js
+++ dados/alunos.js
@@ -45,3 +45,43 @@ const alunosDB = [
     ativo: false
     }
 ]
+
+const emailValido = (email) => {
+    const emailRegExp = new RegExp(/[\w-\.]+@([\w-]+\.)+[\w-]/) // RegExp que captura padrão de email
+    return Boolean(email.match(emailRegExp))
+}
+
+const cadastrarAluno = (nome, sobrenome, email, turma, nascimento, notas, ativo=true) => {
+
+    const a = {
+        nome,
+        sobrenome,
+        email,
+        turma,
+        nascimento,
+        notas,
+        ativo,
+        }
+
+    const validarAluno = (aluno) => {return Object.entries(aluno).every((val) => {
+        try {
+
+            if (!Boolean(String(val[1]).trim())){
+                throw new Error(`O valor de "${val[0]}" não foi preenchido ou foi preenchido incorretamente`)
+            } else {
+                return Boolean(val)
+            }
+        } catch (err) {
+            console.error(`O valor de ${val[0]} não pode ser vazio.`)
+        }
+    })}
+
+    if (validarAluno(a)) {
+        alunosDB.push(a)
+        return true
+    }
+    else {
+        return false
+    }
+}
+
